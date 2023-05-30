c18705ea3952e302c099921de1e65e12593b7602
Pedro Pereira <palex@cc.isel.ipl.pt>
2023-05-30 22:54:36 +0100

    Aula 30 Maio

--- Loops/src/main/kotlin/SumValues2.kt
+++ Loops/src/main/kotlin/SumValues2.kt
@@ -6,9 +6,4 @@ fun main(args: Array<String>) {
     val values = args.map{ it.toInt() }
     println(values)
     println(values.sum())
-
-    val a = arrayOf(1,2,3)
-    val b = a + 10
-    val c = a - 2
-    println(b.toList())
 }
